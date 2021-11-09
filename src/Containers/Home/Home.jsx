e19eb2fb308c7cdb60949c68494fcfa2312b9559
Dave86dev <davidochando1986@gmail.com>
2021-11-09 11:10:49 +0100

    Kick off integracion Sass Scss

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className='fondo basics_column'>
+            <div className="claseCard mb-2"></div>
+            <div className="claseCard ml-4 mt-7"></div>
+            <div className="claseCard mt-1"></div>
+        </div>
+    )
+}
+
+export default Home;
